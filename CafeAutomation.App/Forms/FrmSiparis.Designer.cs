cef36b6559cb79ba302c09f7b1fb0ae9c562bb2a
Emree37 <34719304+Emree37@users.noreply.github.com>
2021-11-17 10:28:57 +0300

    Sipariş Form

--- CafeAutomation.App/Forms/FrmSiparis.Designer.cs
+++ CafeAutomation.App/Forms/FrmSiparis.Designer.cs
@@ -32,7 +32,22 @@ namespace CafeAutomation.App.Forms
             this.lblMasaIsmi = new System.Windows.Forms.Label();
             this.dgvListe = new System.Windows.Forms.DataGridView();
             this.lblToplamTutar = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnFastFood = new System.Windows.Forms.Button();
+            this.btnCorbalar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbUrunler = new System.Windows.Forms.ComboBox();
+            this.txtAdet = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnIptal = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGeriDon = new System.Windows.Forms.Button();
+            this.btnAdisyon = new System.Windows.Forms.Button();
+            this.btnMasayiKapat = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvListe)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblMasaIsmi
@@ -48,11 +63,11 @@ namespace CafeAutomation.App.Forms
             this.dgvListe.AllowUserToAddRows = false;
             this.dgvListe.AllowUserToDeleteRows = false;
             this.dgvListe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvListe.Location = new System.Drawing.Point(287, 95);
+            this.dgvListe.Location = new System.Drawing.Point(287, 69);
             this.dgvListe.Name = "dgvListe";
             this.dgvListe.ReadOnly = true;
             this.dgvListe.RowTemplate.Height = 25;
-            this.dgvListe.Size = new System.Drawing.Size(397, 275);
+            this.dgvListe.Size = new System.Drawing.Size(501, 186);
             this.dgvListe.TabIndex = 1;
             // 
             // lblToplamTutar
@@ -63,17 +78,165 @@ namespace CafeAutomation.App.Forms
             this.lblToplamTutar.Size = new System.Drawing.Size(0, 15);
             this.lblToplamTutar.TabIndex = 2;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnFastFood);
+            this.groupBox1.Controls.Add(this.btnCorbalar);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 242);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kategoriler";
+            // 
+            // btnFastFood
+            // 
+            this.btnFastFood.Location = new System.Drawing.Point(108, 22);
+            this.btnFastFood.Name = "btnFastFood";
+            this.btnFastFood.Size = new System.Drawing.Size(86, 45);
+            this.btnFastFood.TabIndex = 1;
+            this.btnFastFood.Text = "FastFood";
+            this.btnFastFood.UseVisualStyleBackColor = true;
+            this.btnFastFood.Click += new System.EventHandler(this.btnFastFood_Click);
+            // 
+            // btnCorbalar
+            // 
+            this.btnCorbalar.Location = new System.Drawing.Point(7, 23);
+            this.btnCorbalar.Name = "btnCorbalar";
+            this.btnCorbalar.Size = new System.Drawing.Size(84, 45);
+            this.btnCorbalar.TabIndex = 0;
+            this.btnCorbalar.Text = "Çorbalar";
+            this.btnCorbalar.UseVisualStyleBackColor = true;
+            this.btnCorbalar.Click += new System.EventHandler(this.btnCorbalar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 274);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ürün Adı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Adet :";
+            // 
+            // cmbUrunler
+            // 
+            this.cmbUrunler.FormattingEnabled = true;
+            this.cmbUrunler.Location = new System.Drawing.Point(96, 274);
+            this.cmbUrunler.Name = "cmbUrunler";
+            this.cmbUrunler.Size = new System.Drawing.Size(121, 23);
+            this.cmbUrunler.TabIndex = 6;
+            // 
+            // txtAdet
+            // 
+            this.txtAdet.Location = new System.Drawing.Point(96, 303);
+            this.txtAdet.Name = "txtAdet";
+            this.txtAdet.Size = new System.Drawing.Size(121, 23);
+            this.txtAdet.TabIndex = 7;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(96, 332);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(121, 45);
+            this.btnEkle.TabIndex = 8;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnIptal
+            // 
+            this.btnIptal.Location = new System.Drawing.Point(287, 281);
+            this.btnIptal.Name = "btnIptal";
+            this.btnIptal.Size = new System.Drawing.Size(121, 45);
+            this.btnIptal.TabIndex = 9;
+            this.btnIptal.Text = "İPTAL ET";
+            this.btnIptal.UseVisualStyleBackColor = true;
+            this.btnIptal.Click += new System.EventHandler(this.btnIptal_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(414, 281);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(121, 45);
+            this.btnSil.TabIndex = 10;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGeriDon
+            // 
+            this.btnGeriDon.Location = new System.Drawing.Point(541, 281);
+            this.btnGeriDon.Name = "btnGeriDon";
+            this.btnGeriDon.Size = new System.Drawing.Size(121, 45);
+            this.btnGeriDon.TabIndex = 11;
+            this.btnGeriDon.Text = "GERİ DÖN";
+            this.btnGeriDon.UseVisualStyleBackColor = true;
+            this.btnGeriDon.Click += new System.EventHandler(this.btnGeriDon_Click);
+            // 
+            // btnAdisyon
+            // 
+            this.btnAdisyon.Location = new System.Drawing.Point(668, 281);
+            this.btnAdisyon.Name = "btnAdisyon";
+            this.btnAdisyon.Size = new System.Drawing.Size(121, 45);
+            this.btnAdisyon.TabIndex = 12;
+            this.btnAdisyon.Text = "ADİSYON";
+            this.btnAdisyon.UseVisualStyleBackColor = true;
+            this.btnAdisyon.Click += new System.EventHandler(this.btnAdisyon_Click);
+            // 
+            // btnMasayiKapat
+            // 
+            this.btnMasayiKapat.Location = new System.Drawing.Point(668, 332);
+            this.btnMasayiKapat.Name = "btnMasayiKapat";
+            this.btnMasayiKapat.Size = new System.Drawing.Size(121, 45);
+            this.btnMasayiKapat.TabIndex = 13;
+            this.btnMasayiKapat.Text = "MASAYI KAPAT";
+            this.btnMasayiKapat.UseVisualStyleBackColor = true;
+            this.btnMasayiKapat.Click += new System.EventHandler(this.btnMasayiKapat_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(668, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 15);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Toplam Tutar :";
+            // 
             // FrmSiparis
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnMasayiKapat);
+            this.Controls.Add(this.btnAdisyon);
+            this.Controls.Add(this.btnGeriDon);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnIptal);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtAdet);
+            this.Controls.Add(this.cmbUrunler);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.lblToplamTutar);
             this.Controls.Add(this.dgvListe);
             this.Controls.Add(this.lblMasaIsmi);
             this.Name = "FrmSiparis";
             this.Text = "FrmSiparis";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmSiparis_FormClosed);
+            this.Load += new System.EventHandler(this.FrmSiparis_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvListe)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -84,5 +247,19 @@ namespace CafeAutomation.App.Forms
         public System.Windows.Forms.Label lblMasaIsmi;
         private System.Windows.Forms.DataGridView dgvListe;
         private System.Windows.Forms.Label lblToplamTutar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnFastFood;
+        private System.Windows.Forms.Button btnCorbalar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbUrunler;
+        private System.Windows.Forms.TextBox txtAdet;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnIptal;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGeriDon;
+        private System.Windows.Forms.Button btnAdisyon;
+        private System.Windows.Forms.Button btnMasayiKapat;
+        private System.Windows.Forms.Label label3;
     }
 }
--- CafeAutomation.App/Forms/FrmSiparis.cs
+++ CafeAutomation.App/Forms/FrmSiparis.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,6 +19,12 @@ namespace CafeAutomation.App.Forms
             InitializeComponent();
         }
 
+        private void GridAyari()
+        {
+            dgvListe.ReadOnly = true;
+
+        }
+
         private List<SiparisDetay> SiparisDetaylarKategori = new List<SiparisDetay>();
         public void SiparisleriGetir()
         {
@@ -29,6 +36,7 @@ namespace CafeAutomation.App.Forms
             dgvListe.ClearSelection();
             MasaKontrol();
         }
+
 
         public void ToplamTutarHesaplama()
         {
@@ -81,6 +89,192 @@ namespace CafeAutomation.App.Forms
 
 
 
+        }
+
+        private void FrmSiparis_Load(object sender, EventArgs e)
+        {
+            UrunContext.Load();
+            GridAyari();
+            ToplamTutarHesaplama();
+            dgvListe.ClearSelection();
+            MasaKontrol();
+        }
+
+        private void btnFastFood_Click(object sender, EventArgs e)
+        {
+            cmbUrunler.Items.Clear();
+            foreach (var urun in UrunContext.Urunler)
+            {
+                if (urun.Kategori == "FastFood")
+                {
+                    cmbUrunler.Items.Add(urun);
+                }
+            }
+        }
+
+        private void btnCorbalar_Click(object sender, EventArgs e)
+        {
+            cmbUrunler.Items.Clear();
+            foreach (var urun in UrunContext.Urunler)
+            {
+                if (urun.Kategori == "Çorbalar")
+                {
+                    cmbUrunler.Items.Add(urun);
+                }
+
+            }
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            Urun seciliUrun = (Urun)cmbUrunler.SelectedItem;
+            SiparisDetay siparisDetay = new SiparisDetay
+            {
+                UrunAdi = seciliUrun.UrunAdi,
+                Fiyat = seciliUrun.Fiyat,
+                Adet = txtAdet.Text,
+                TutarTL = (Int32.Parse(seciliUrun.Fiyat) * Int32.Parse(txtAdet.Text)).ToString(),
+                MasaIsmi = lblMasaIsmi.Text
+            };
+            SiparisDetayContext.SiparisDetaylar.Add(siparisDetay);
+            SiparisDetayContext.Save();
+            SiparisleriGetir();
+
+            dgvListe.ClearSelection();
+            ToplamTutarHesaplama();
+            MasaKontrol();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            var selected = dgvListe.SelectedRows[0];
+            seciliMasa = selected.Cells[4].Value.ToString();
+            var secili = SiparisDetayContext.SiparisDetaylar.Where(x => x.MasaIsmi == seciliMasa).ToList()[selected.Index];
+            SiparisDetayContext.SiparisDetaylar.Remove(secili);
+            SiparisDetayContext.Save();
+            SiparisleriGetir();
+            ToplamTutarHesaplama();
+            MasaKontrol();
+        }
+
+        private void btnIptal_Click(object sender, EventArgs e)
+        {
+            seciliMasa = lblMasaIsmi.Text;
+            for (int i = 0; i < SiparisDetayContext.SiparisDetaylar.Count; i++)
+            {
+                if (SiparisDetayContext.SiparisDetaylar[i].MasaIsmi == seciliMasa)
+                {
+                    SiparisDetayContext.SiparisDetaylar.Remove(SiparisDetayContext.SiparisDetaylar[i]);
+                    SiparisDetayContext.Save();
+                    i--;
+                }
+            }
+            MasaKontrol();
+            Close();
+        }
+
+        private void btnGeriDon_Click(object sender, EventArgs e)
+        {
+            MasaKontrol();
+            Close();
+        }
+
+        private void FrmSiparis_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ButonlarıGöster();
+            dgvListe.ClearSelection();
+        }
+
+        private void ButonlarıGöster()
+        {
+            foreach (Control item in this.MdiParent.Controls)
+            {
+                if (item is Button)
+                {
+                    item.Visible = true;
+                }
+            }
+        }
+
+        private void btnAdisyon_Click(object sender, EventArgs e)
+        {
+            using (System.IO.StreamWriter file =
+         new System.IO.StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + $"/yazdir{lblMasaIsmi.Text}.txt", false))
+            {
+                file.WriteLine("   ----------  CAFE AUTOMATİON  ----------   ");
+                file.WriteLine("MASA İSMİ: " + lblMasaIsmi.Text);
+                file.WriteLine("--- Masa Detay : ---");
+                file.WriteLine(" |     Ürün    |    Fiyat    |     Adet   ");
+                string lines = "";
+
+                for (int row = 0; row < SiparisDetayContext.SiparisDetaylar.Count; row++)
+                {
+                    for (int col = 0; col < 3; col++)
+                    {
+
+                        lines = lines + " | " + dgvListe.Rows[row].Cells[col].Value.ToString();
+                        if (col == 2)
+                        {
+
+                            file.WriteLine(lines);
+                            lines = "";
+                        }
+                    }
+
+                }
+                file.WriteLine(" ------------------------");
+                file.WriteLine(" Toplam :" + lblToplamTutar.Text.ToString());
+
+            }
+
+
+            var pi = new ProcessStartInfo("yazdir.txt");
+            pi.UseShellExecute = true;
+            pi.Verb = "print";
+            var process = System.Diagnostics.Process.Start(pi);
+
+            MessageBox.Show("YAZDIRILIYOR...");
+        }
+
+        private void btnMasayiKapat_Click(object sender, EventArgs e)
+        {
+            Rapor rapor = new Rapor()
+            {
+                MasaIsmi = lblMasaIsmi.Text,
+                //Tarih = DateTime.Now.ToShortDateString(),
+                TarihGun = DateTime.Now.ToString("MM/dd/yyyy"),
+                TarihSaat = DateTime.Now.ToString("HH:mm:ss"),
+                ToplamTutar = lblToplamTutar.Text
+            };
+            RaporContext.Raporlar.Add(rapor);
+            RaporContext.Save();
+
+            seciliMasa = lblMasaIsmi.Text;
+            for (int i = 0; i < SiparisDetayContext.SiparisDetaylar.Count; i++)
+            {
+                if (SiparisDetayContext.SiparisDetaylar[i].MasaIsmi == seciliMasa)
+                {
+                    KapatilanSiparisler kapatilanSiparisler = new KapatilanSiparisler()
+                    {
+                        UrunAdi = SiparisDetayContext.SiparisDetaylar[i].UrunAdi,
+                        Fiyat = SiparisDetayContext.SiparisDetaylar[i].Fiyat,
+                        Adet = SiparisDetayContext.SiparisDetaylar[i].Adet,
+                        TutarTL = SiparisDetayContext.SiparisDetaylar[i].TutarTL,
+                        MasaIsmi = SiparisDetayContext.SiparisDetaylar[i].MasaIsmi,
+                        TarihGun = DateTime.Now.ToString("MM/dd/yyyy"),
+                        TarihSaat = DateTime.Now.ToString("HH:mm:ss")
+                    };
+                    KapatilanSiparislerContext.KapatilanSiparisler.Add(kapatilanSiparisler);
+                    KapatilanSiparislerContext.Save();
+
+                    SiparisDetayContext.SiparisDetaylar.Remove(SiparisDetayContext.SiparisDetaylar[i]);
+                    SiparisDetayContext.Save();
+
+                    i--;
+                }
+            }
+            MasaKontrol();
+            Close();
         }
     }
 }
